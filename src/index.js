16536d654647c101bfc304b380f74aef85f52835
Rafał Olewicz <namerafal75@gmail.com>
2024-06-15 10:16:28 +0200

    file with global style was renamed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { GlobalStyles } from './styled';
+import { GlobalStyles } from './GlobalStyle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

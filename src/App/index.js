d3c5e57e9d6e7fff182eb93260ef93e59c6bb62b
Rafał Olewicz <namerafal75@gmail.com>
2024-10-01 21:58:25 +0200

    API requests for currencies have been added

--- src/App/index.js
+++ src/App/index.js
@@ -1,35 +1,42 @@
 import { useState } from 'react';
 import { AppContainer } from './styled';
 import { Header } from './Header';
-import { currencies } from './currencies';
+import useCurrencyRequest from '../useCurrencyRequest';
 import Form from './Form';
 
 function App() {
   const [result, setResult] = useState({ sourceAmount: null, amountToCurrency: null, selectedCurrency: null });
   const [currencyRate, setCurrencyRate] = useState(null);
+  const { rates, loading, error } = useCurrencyRequest("");
 
   const calculateResult = (amount, selectedCurrency) => {
-    const currencyRate = currencies.find(({ short }) => short === selectedCurrency);
+    const currencyRate = rates[selectedCurrency]?.value;
 
     setResult({
       sourceAmount: +amount,
-      amountToCurrency: currencyRate && !isNaN(amount)
-        ? amount * currencyRate.rate
-        : null,
+      amountToCurrency: amount * currencyRate,
       selectedCurrency,
     });
+
   }
 
   return (
     <AppContainer>
-      <Header title={"Przeliczanie walut na PLN"} />
-      <Form
-        result={result}
-        currencyRate={currencyRate}
-        calculateResult={calculateResult}
-        setResult={setResult}
-        setCurrencyRate={setCurrencyRate}
-      />
+      <Header title={"Przeliczanie PLN na waluty"} />
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>error: {error}</div>
+      ) : (
+        <Form
+          result={result}
+          currencyRate={currencyRate}
+          calculateResult={calculateResult}
+          setResult={setResult}
+          setCurrencyRate={setCurrencyRate}
+          rates={rates}
+        />
+      )}
     </AppContainer>
   );
 }

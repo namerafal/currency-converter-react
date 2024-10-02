31cf8ae1adaea4ddab3c08af3a5da12cce225fcc
Rafał Olewicz <namerafal75@gmail.com>
2024-10-02 19:56:15 +0200

    styles created for the LoadingField

--- src/App/styled.js
+++ src/App/styled.js
@@ -4,3 +4,16 @@ export const AppContainer = styled.div`
     text-align: center;
     margin: 10px;
 `;
+
+export const LoadingField = styled.fieldset`
+    min-height:300px;
+    text-align: center;
+    border: 1px solid ${({ theme }) => theme.color.easternBlue};
+    padding: 20px 30px;
+    border-radius: 8px;
+    box-shadow: 0px 0px 15px ${({ theme }) => theme.color.black};
+    background-color: ${({ theme }) => theme.color.alabaster};
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+`;

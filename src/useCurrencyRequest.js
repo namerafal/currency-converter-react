6ae87e53033d343aa3065784b064ac6355d48247
Rafał Olewicz <namerafal75@gmail.com>
2024-10-06 21:42:39 +0200

    Error correction from the loading test

--- src/useCurrencyRequest.js
+++ src/useCurrencyRequest.js
@@ -11,7 +11,7 @@ const useCurrencyRequest = () => {
     const fetchRates = async () => {
       try {
 
-        const response = await axios.get('currenciets.json');
+        const response = await axios.get('currencies.json');
         // const response = await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_FVxTKQHW27c4Yi5Ksu20Ymx7ihSW6YKRrdg11jmS&currencies=EUR%2CUSD%2CCAD%2CGBP&base_currency=PLN');
 
         const { meta, data } = response.data;
